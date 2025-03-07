e41bf92e95fe5462153957be08d1a84a8720984a
Clement Mathole <Clementmathole003@gmail.com>
2025-03-07 16:54:03 +0200

    Update Url.cs

--- Models/Url.cs
+++ Models/Url.cs
@@ -2,21 +2,21 @@ namespace URL_Shortener.Models
 {
     public class Url
     {
-        //
-        //Name : Url(string originalUrl, string shortUrl)
-        //Purpose : Constructor to initialize the original and short URL
-        //Re-use : None
-        //Input Parameters : string originalUrl
-        // - The original URL to be shortened
-        //string shortUrl
-        // - The shortened URL
-        //Output Type : None
-        //
         public string OriginalUrl { get; set; }
         public string ShortUrl { get; set; }
 
         public Url(string originalUrl, string shortUrl)
         {
+            //
+            //Name    : Url(string originalUrl, string shortUrl)
+            //Purpose : Constructor to initialize the original and short URL
+            //Re-use  : None
+            //Input Parameters : string originalUrl
+            // - The original URL to be shortened
+            //string shortUrl
+            // - The shortened URL
+            //Output Type : None
+            //
             OriginalUrl = originalUrl;
             ShortUrl = shortUrl;
         } //end Url Constractor
